e1a735ae314aa4ce451a4ecd24dbf91d8f17961b
nickbar01234 <nickbar01234@gmail.com>
2021-04-09 08:00:55 +0700

    particle

--- particle.py
+++ particle.py
@@ -1,56 +1,48 @@
+'''
+* Object representation of a particle. 
+'''
+
 import numpy as np 
 
 class Particle:
-    def __init__(self, collision_type: str, x: float = None, y: float = None, 
-                 vx: float = None, vy: float = None, mass: float = None):
-        
-        assert collision_type in ["1D", "2D"], \
-        "'Collisions are only modelled for 1D and 2D kinematics.'"
-        assert mass is not None, \
-        "'Objects cannot be massless."
+    def __init__(self, mode: str, length: float, width: float, mass: float,
+                 xpos: float = None, ypos: float = None, v_x: float = None, 
+                 v_y: float = None):
 
-        self.x = []
-        self.y = []
-        self.vx = []
-        self.vy = []
+        assert mode in ["1D", "2D"], "Mode must be 1D or 2D"
+        assert (length and width) > 0, "Particle must have size"
+        assert mass > 0, "Particle must have mass"
+
+        self.length = length 
+        self.width = width 
         self.mass = mass 
-        
+
+        self.xpos = xpos 
+        self.v_x = v_x 
+
+        if mode == "2D":
+            self.ypos = ypos 
+            self.v_y = v_y 
+    
     @property 
     def x(self):
-        return self.x[-1]
+        return self.xpos 
     
-    @x.setter
-    def x(self, value):
-        self.x.append(value)
-    
-    @property 
-    def deltaX(self):
-        if len(x) == 1:
-            return self.x[-1]
-        return np.substract(*self.x[-1:-3:-1])
-
-    @property 
-    def y(self):
-        return self.y[-1]
-    
-    @y.setter 
-    def y(self, value):
-        self.y.append(value)
+    @x.setter 
+    def x(self, value: int):
+        self.xpos = value 
     
     @property 
     def vx(self):
-        return self.vx[-1]
+        return self.v_x 
     
-    @vx.setter
+    @vx.setter 
     def vx(self, value):
-        self.vx.append(value)
+        self.v_x = value 
 
-    @property 
-    def vy(self):
-        return self.vy[-1]    
-
-    @vy.setter
-    def vy(self, value):
-        self.vy.append(value)
-    
-
+    def __overlap(self, other: Particle):
+        '''
+        * Check if two particles are overlapped. 
+        '''
+        return other.x <= self.x and self.x - self.width <= other.x 
+        
--- simulation.py
+++
@@ -1,7 +0,0 @@
-from system import *
-
-class Simulation:
-    def __init__(self, length: float, width: float, system: System):
-        self.length = length 
-        self.width = width 
-        self.system = system 
